2fd15ebff11522c626fdb1e5801b8f2fd95663f5
Quincy Arthur <quincya@nib-bahamas.com>
2020-04-08 12:13:09 -0400

    correcting script to get manual check files

--- modules/application/scripts/get_pending_reissued_manual_payments.sql
+++ modules/application/scripts/get_pending_reissued_manual_payments.sql
@@ -1,8 +1,6 @@
 select  pmt.pmt_amt,to_char(pmt.pmt_date,'DD'),to_char(pmt.pmt_date,'MM'),
         to_char(pmt.pmt_date,'YYYY'),app.eeni#,
-       upper(app.first_name) || ' ' || upper(app.last_name),
-       (select client.self_emp_ueb_chk_run_batch_seq.currval from dual),
-       pmt.pmt_id
+       upper(app.first_name) || ' ' || upper(app.last_name)
 from client.self_emp_ueb_pmt_master pmt
 inner join client.self_emp_ueb_app app on app.app_id = pmt.app_id
 where pmt.pmt_type = 'Check'
